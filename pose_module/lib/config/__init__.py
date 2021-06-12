701764355527d87f4a4edda2e158129206a7f342
AllenYLJiang <AllenYLJiang@outlook.com>
2021-06-12 16:39:43 +0800

    Add files via upload

---
+++ pose_module/lib/config/__init__.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from .default import _C as cfg
+from .default import update_config
+from .default import update_config_dict
+from .models import MODEL_EXTRAS
---
+++ pose_module/lib/models/__init__.py
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pose_module.lib.models.pose_resnet
+import pose_module.lib.models.pose_hrnet
+import pose_module.lib.models.lpn
